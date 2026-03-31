061305c7165a410d31abbfabe90861a12bfb5266
Rayane Benabdeljalil <benabdeljalilrayane1@gmail.com>
2026-03-31 15:40:35 +0200

    Set BrowserRouter basename to /MyPortfolio
    Pass basename="/MyPortfolio" to BrowserRouter in src/App.tsx so client-side routes resolve correctly when the app is hosted from the /MyPortfolio subdirectory.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+        <BrowserRouter basename="/MyPortfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/project/:projectId" element={<ProjectDetail />} />
